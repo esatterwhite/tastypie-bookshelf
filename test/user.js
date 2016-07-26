2d1b4ad707245311abc836bb46357f13e104ad02
Eric Satterwhite <esatterwhite@wi.rr.com>
2016-07-26 17:24:22 -0500

    Quick model set up for Testing

---
+++ test/user.js
@@ -0,0 +1,21 @@
+'use strict';
+var bookshelf = require('bookshelf')(require('./database'))
+  , User
+  , UserFriend
+
+UserFriend = bookshelf.Model.extend({
+    tableName: 'auth_user_friend',
+    idAttribute:'auth_user_friend_id'
+});
+
+User = bookshelf.Model.extend({
+    tableName: 'auth_user',
+    idAttribute:'auth_user_id',
+    friends: function(){
+        return this.belongsToMany(User, 'auth_user_id')
+                    .through(UserFriend,'auth_user_friend_id','auth_friend_id');
+    }
+});
+
+
+module.exports = User;
