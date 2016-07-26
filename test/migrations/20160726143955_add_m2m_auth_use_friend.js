7b89ec706ddbafe949758a9752b5526e28aa990c
Eric Satterwhite <esatterwhite@wi.rr.com>
2016-07-26 18:09:08 -0500

    fix the pk in the migration for friends m2m table

--- test/migrations/20160726143955_add_m2m_auth_use_friend.js
+++ test/migrations/20160726143955_add_m2m_auth_use_friend.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('auth_user_friend', function( table ){
   	table.engine('innodb');
-  	table.increments('id');
+	table.increments('auth_user_friend_id');
   	table.integer('auth_user_id').notNullable().unsigned();
   	table.integer('auth_friend_id').notNullable().unsigned();
   	table.foreign('auth_user_id').references('auth_user.auth_user_id');
